404f933859015a584ee962d6df2c9920e46a6058
CJButton <gsyme1907@gmail.com>
2017-02-01 15:31:07 -0800

    basic devise auth installed

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@
 
 Rails.application.routes.draw do
 
-
-
-
-
+  devise_for :users
   root to: 'static_pages#root'
+
+  # namespace :api, defaults: {format: :json} do
+  #   resource :user, only: [:create]
+  #   resource :session, only: [:create, :destroy, :show]
+  #   resources :reviews, only: [:index, :create, :destroy, :update, :show]
+  # end
 
 end
