de2efee7a99f698a0485a9da0849dd22455d56b3
CJButton <gsyme1907@gmail.com>
2017-02-01 21:50:25 -0800

    have a index component showing

---
+++ frontend/components/auth/auth_container.jsx
@@ -0,0 +1,24 @@
+
+
+import {connect} from 'react-redux';
+import {logout, login, signup} from '../../actions/session_actions';
+import AuthForm from './auth_component';
+
+
+// if user is logged in, then redirect them elsewhere
+const mapStateToProps = ({session}) => ({
+  currentUser: session.currentUser,
+  errors: session.errors,
+  loggedIn: Boolean(session.currentUser)
+});
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logout()),
+  login: user => dispatch(login(user)),
+  signup: user => dispatch(signup(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AuthForm);
