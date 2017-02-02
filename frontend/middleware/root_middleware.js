43ee25b9f23bbbb59f0356497bf3ba0a173db1fc
CJButton <gsyme1907@gmail.com>
2017-02-01 18:36:05 -0800

    basic sessions/new user work is finished

---
+++ frontend/middleware/root_middleware.js
@@ -0,0 +1,11 @@
+
+
+import { applyMiddleware } from 'redux';
+
+import SessionMiddleware from './session_middleware';
+
+const RootMiddleware = applyMiddleware(
+  SessionMiddleware
+);
+
+export default RootMiddleware;
