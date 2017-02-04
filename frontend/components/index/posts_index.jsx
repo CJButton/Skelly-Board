fb9395e50f3029a350db8093330c6530006c73d0
CJButton <gsyme1907@gmail.com>
2017-02-03 20:45:33 -0800

    created basic modal for adding a post

--- frontend/components/index/posts_index.jsx
+++ frontend/components/index/posts_index.jsx
@@ -9,8 +9,20 @@ class PostsIndex extends React.Component{
     super(props);
 
     this.state = {
-
+      addModal: false,
     };
+  }
+
+  closeModal() {
+    this.setState({
+      addModal: false
+    });
+  }
+
+  openAddModal() {
+    this.setState({
+      addModal: true
+    });
   }
 
 
@@ -19,7 +31,9 @@ class PostsIndex extends React.Component{
     console.log(this.state);
     return (
     <div className="postsIndexWrapper">
-      {this.props.posts.map((post, i) => {
+      <div className="postsIndexInnerWrapper">
+        <button className="createPost button">Make a Post</button>
+      {this.props.posts.reverse().map((post, i) => {
         return(
           <div key={i} className="indexPost">
             <a href={`#/posts/${post.id}`}>
@@ -30,6 +44,55 @@ class PostsIndex extends React.Component{
           </div>
         );
       })}
+      </div>
+
+      <Modal className="addModal"
+          isOpen={this.state.addModal}
+          contentLabel="Modal2">
+          <div className="addFormTop">
+          <h1 className="addFormTitle">Create your own Post:</h1>
+          <button className="closePostButton"
+            onClick={this.closeModal.bind(this)}>X</button>
+          </div>
+          <div className="formTop">
+            <div className="formTopLeft">
+              {this.state.userReview === undefined ? null :
+                <p className="addUsername">
+                  {this.state.userReview.username}</p>}
+              <div className="addFormTop">
+                <StarRatingComponent
+                  className="starRating"
+                  name="rater"
+                  starCount={5}
+                  value={this.state.rating}
+                  onStarClick={this.onStarClick.bind(this)}/>
+              </div>
+              <p className="addComicTitle">
+                {this.props.manga === undefined ? null : this.props.manga.title}</p>
+            </div>
+          </div>
+
+        <form className="editFormModal"
+          onSubmit={this.handleSubmit.bind(this)}>
+          <p className="editTitle">Title</p>
+          <input className="editReview-text"
+            type="text"
+            onChange={this.handleTitle}
+            value={this.state.title}></input>
+
+          <p className="editDescription">Description</p>
+          <textarea className="editReview-textarea"
+            onChange={this.handleText}
+            value={this.state.text}></textarea>
+
+          <button className="review-submit button"
+            type="submit">Submit!</button>
+        </form>
+
+        </Modal>
+
+
+
     </div>
 
     );
